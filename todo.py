50bcabea1441415421d1917ec270375d6a0c5fc0
atokar13ciber <atokar@cibernos.com>
2024-01-20 17:24:14 +0100

    Update todo.py
    status codes added

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Response, status
 from models import Task
 
 
@@ -7,20 +7,38 @@ todo_list = []
 
 
 @todo_router.get('/read-tasks')
-def read_tasks():
+def read_tasks(response: Response):
+    response.status_code = status.HTTP_200_OK
+    if len(todo_list) == 0:
+        response.status_code = status.HTTP_404_NOT_FOUND
     return {'todo_list': todo_list}
 
 @todo_router.post('/create-task')
-def create_task(task: Task):
+def create_task(response: Response, task: Task):
+    for todo in todo_list:
+        if todo.id == task.id:
+            response.status_code = status.HTTP_409_CONFLICT
+            return False
     todo_list.append(task)
+    response.status_code = status.HTTP_201_CREATED
     return {'todo_list': todo_list}
 
-@todo_router.put('/update-task/{id}/{task}')
-def update_task(id: int, task: str):
-    todo_list[id] = task
-    return {'todo_list': todo_list}
+@todo_router.put('/update-task/{id}')
+def update_task(response: Response, id: int, task: Task):
+    for i in range(len(todo_list)):
+        if todo_list[i].id == id:
+            todo_list[i] = task
+            response.status_code = status.HTTP_200_OK
+            return {'todo_list': todo_list}
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {'Error': 'Element with this id does not found'}
 
 @todo_router.delete('/delete-task/{id}')
-def delete_task(id: int):
-    del todo_list[id]
-    return {'todo_list': todo_list}
+def delete_task(response: Response, id: int):
+    for i in range(len(todo_list)):
+        if todo_list[i].id == id:
+            del todo_list[i]
+            response.status_code = status.HTTP_200_OK
+            return {'todo_list': todo_list}
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {'Error': 'Element with this id does not found'}
