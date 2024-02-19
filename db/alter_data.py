3a0bbdaa9eb91a090242428c442774056ea30070
atokar13ciber <atokar@cibernos.com>
2024-02-19 21:56:21 +0100

    db_qry and alter_data

--- db/alter_data.py
+++ db/alter_data.py
@@ -16,3 +16,29 @@ def update_status(db, id, status):
 def delete_task(db, id):
     res = db.todos.delete_one({'_id': ObjectId(id)})
     return res.deleted_count
+
+def create_user(db, user):
+    user = dict(user)
+    db.users.insert_one(user)
+
+def update_user(db, id, user):
+    user = dict(user)
+    res = db.users.update_one({'_id': ObjectId(id)}, {'$set': user})
+    return res.modified_count 
+
+def delete_user(db, id):
+    res = db.users.delete_one({'_id': ObjectId(id)})
+    return res.deleted_count
+
+def create_category(db, category):
+    category = dict(category)
+    db.categories.insert_one(category)
+
+def update_category(db, id, category):
+    category = dict(category)
+    res = db.categories.update_one({'_id': ObjectId(id)}, {'$set': category})
+    return res.modified_count 
+
+def delete_category(db, id):
+    res = db.categories.delete_one({'_id': ObjectId(id)})
+    return res.deleted_count
--- db/db_qry.py
+++ db/db_qry.py
@@ -14,3 +14,31 @@ def todo(db, id):
     if res is not None:
         res['_id'] = str(res['_id'])
     return res
+
+def users(db):
+    cursor = db.users.find()
+    user_list = []
+    for document in cursor:
+        document['_id'] = str(document['_id'])
+        user_list.append(document)
+    return user_list
+
+def user(db, id):
+    res = db.users.find_one({'_id': ObjectId(id)})
+    if res is not None:
+        res['_id'] = str(res['_id'])
+    return res
+
+def categories(db):
+    cursor = db.categories.find()
+    category_list = []
+    for document in cursor:
+        document['_id'] = str(document['_id'])
+        category_list.append(document)
+    return category_list
+
+def category(db, id):
+    res = db.categories.find_one({'_id': ObjectId(id)})
+    if res is not None:
+        res['_id'] = str(res['_id'])
+    return res
